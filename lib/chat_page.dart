e5f6491d1a06f55beeb7dd85f2db32b52bde55d7
asirisam <asirisam@gmail.com>
2025-08-23 23:54:48 +0100

    Restore chat_page.dart to commit 43f9535

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,11 +1,8 @@
 import 'package:chat_app/login_page.dart';
-import 'package:chat_app/widgets/chat_bubble.dart';
-import 'package:chat_app/widgets/chat_input.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
-
 
   @override
   Widget build(BuildContext context) {
@@ -31,18 +28,112 @@ class ChatPage extends StatelessWidget {
             flex: 1,
             child: ListView(
               children: [
-                ChatBubble(
-                    alignment: Alignment.centerLeft,
-                    message: "Hello, this is Asiri!"
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    padding: EdgeInsets.all(24),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text('Hi, this is your message', style: TextStyle(fontSize: 20, color: Colors.white),),
+                        Image.network(
+                            'https://img.itch.zone/aW1nLzU2MTA3NTUucG5n/original/tcdxAa.png',
+                          height: 200,
+                        )
+                      ],
+                    ),
+                    margin: EdgeInsets.all(50),
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(12),
+                        topRight: Radius.circular(12),
+                        bottomLeft: Radius.circular(12),
+                      )
+                    ),
+                  ),
                 ),
-                ChatBubble(
-                    alignment: Alignment.centerRight,
-                    message: "Hi!!"
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    padding: EdgeInsets.all(24),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text('Hi, this is your message', style: TextStyle(fontSize: 20, color: Colors.white),),
+                        Image.network(
+                          'https://img.itch.zone/aW1nLzU2MTA3NTUucG5n/original/tcdxAa.png',
+                          height: 200,
+                        )
+                      ],
+                    ),
+                    margin: EdgeInsets.all(50),
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(12),
+                          topRight: Radius.circular(12),
+                          bottomLeft: Radius.circular(12),
+                        )
+                    ),
+                  ),
                 ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    padding: EdgeInsets.all(24),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text('Hi, this is your message', style: TextStyle(fontSize: 20, color: Colors.white),),
+                        Image.network(
+                          'https://img.itch.zone/aW1nLzU2MTA3NTUucG5n/original/tcdxAa.png',
+                          height: 200,
+                        )
+                      ],
+                    ),
+                    margin: EdgeInsets.all(50),
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(12),
+                          topRight: Radius.circular(12),
+                          bottomLeft: Radius.circular(12),
+                        )
+                    ),
+                  ),
+                ),
+            
+            
               ],
             ),
           ),
-          ChatInput(),
+          Container(
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  onPressed: (){},
+                  icon: Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  ),
+                ),
+                IconButton(
+                  onPressed: (){},
+                  icon: Icon(
+                    Icons.send,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.vertical(top: Radius.circular(20))
+            ),
+          )
         ],
       ),
     );
