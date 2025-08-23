c1a5197c054f44fccb16a3ef513395194a318f42
asirisam <asirisam@gmail.com>
2025-08-23 23:50:39 +0100

    commit after ChatInput code snippet issue

---
+++ lib/widgets/chat_bubble.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ChatBubble extends StatelessWidget {
+  final String message;
+  final Alignment alignment;
+  const ChatBubble({Key? key, required this.alignment, required this.message})
+      : super(key: key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: alignment,
+      child: Container(
+        padding: EdgeInsets.all(24),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              '$message',
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white
+              ),
+            ),
+            Image.network(
+              'https://img.itch.zone/aW1nLzU2MTA3NTUucG5n/original/tcdxAa.png',
+              height: 200,
+              width: 200,
+            )
+          ],
+        ),
+        margin: EdgeInsets.all(50),
+        decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(12),
+              topRight: Radius.circular(12),
+              bottomLeft: Radius.circular(12),
+            )
+        ),
+      ),
+    );
+  }
+}
